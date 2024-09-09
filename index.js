f9a68a1a93b83629994babad22d426078b053c22
GG-Pharaoh <oxdiablo20@gmail.com>
2024-09-09 08:26:54 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+//Code to write basic node js backend
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+
+//Add Middleware
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+})
+
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    const result = Joi.validate(req.body, schema);
+    console.log(result);
+
+    if(!req.body.name || req.body.name.length < 3){
+        // 400 Bad request 
+        res.status(400).send('Name is required and should be minimum 3 characters.');
+        return; 
+    }
+
+    const course = {
+        id: courses.length + 1, 
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+
+app.get('/api/posts/:year/:month', (req, res) => {
+   const course =  courses.find(c => c.id === parseInt(req.params.id));
+   if(!course) res.status(404).send('The course with the given ID was not found');
+   res.send(course);
+})
+
+app.get('/api/courses/:id', (req, res) => {
+    const courseId = parseInt(req.params.id);
+    const course = courses.find(c => c.id === courseId);
+    if (!course) {
+      res.status(404).send('The course with the given ID was not found');
+    } else {
+      res.send(course);
+    }
+  });
+
+  
+
+
+
+
+//PORT
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
